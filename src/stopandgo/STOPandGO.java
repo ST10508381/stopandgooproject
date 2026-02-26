9f1d9b944ad460d87166aad9b9353a1d58534001
ST10508381 <nkomoangel853@gmail.com>
2026-02-26 08:34:53 +0200

    final commit

---
+++ src/stopandgo/STOPandGO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package stopandgo;
+
+/**
+ *
+ * @author Angel Nkomo
+ */
+public class STOPandGO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
